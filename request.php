d160c00faa1b47c2717ec234d3b9ea0589bee8f3
hamzatarif125 <68009988+hamzatarif125@users.noreply.github.com>
2021-04-06 21:39:50 -0700

    kjkj

--- request.php
+++ request.php
@@ -1,7 +1,9 @@
 <?php
 if(isset($_POST['boxColor']))
 {
+    ob_start();
     setcookie("boxColor", $_POST['boxColor'], time() + (86400 * 8), "/");   
+    ob_end_flush();  
     echo $_COOKIE["boxColor"];
 }
 ?>
