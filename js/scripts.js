122ad1d338bd720eefebc140263105f8e16232bd
hamzatarif125 <68009988+hamzatarif125@users.noreply.github.com>
2021-04-07 11:09:05 -0700

    ijkj

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,36 @@
 
-
-
 // when color input change then this function is used to change box color and set cookie 
 function colorPicked()
 {
     var color = document.getElementById("colorInput").value;
     document.getElementById("colorBox").style.backgroundColor = color;
 
-    //calling function to set cookie with name of boxColor & passing input value with 8 days expiry for cookies
-    localStorage.setItem("boxColor", color);
+    $.ajax({
+        type: "POST",
+        url: "./request.php", 
+        data:{
+                color: color
+        },
+        success: function(response){
+        }
+        });
+        
 }
 
 // when page loads then this function is called to set color
 function loadCookieValues()
 {
-    var color = localStorage.getItem("boxColor");
+    var color = "";
+    $.ajax({
+        type: "GET",
+        url: "./request.php", 
+        data:{
+                getColor: "Yes"
+        },
+        success: function(response){
+                color = response;
+        }
+        });
     if(color!=null && color!="")
     {
         document.getElementById("colorBox").style.backgroundColor = color;
--- request.php
+++ request.php
@@ -1,8 +1,15 @@
 <?php
-session_start();
 if(isset($_POST['boxColor']))
 {
-    $_SESSION['boxColorKey'] = $_POST['boxColor'];
-    echo $_SESSION['boxColorKey'];
+    $file = fopen("colorFile.txt","w");
+    echo fwrite($file,$_POST['boxColor']);
+    fclose($file);
+}
+
+if($_REQUEST['getColor']=="Yes")
+{
+    $myfile = fopen("colorFile.txt", "r") or die("Unable to open file!");
+    echo fread($myfile,filesize("colorFile.txt"));
+    fclose($myfile);
 }
 ?>
