e8cd0d31cbc7357d36ab82dfce7de2be1cfae17b
hamzatarif125 <68009988+hamzatarif125@users.noreply.github.com>
2021-04-06 21:58:35 -0700

    kjkjk

--- request.php
+++ request.php
@@ -2,6 +2,6 @@
 if(isset($_GET['boxColor']))
 {
     setcookie("boxColor", $_GET['boxColor'], time() + (86400 * 8), "/") or die('unable to create cookie');
-    header("https://softaims-test.herokuapp.com");  
+    header("Location: https://softaims-test.herokuapp.com");  
 }
 ?>
