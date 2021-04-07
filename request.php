90b581abcfdb735727aaf8af00ea79a4643f8af8
hamzatarif125 <68009988+hamzatarif125@users.noreply.github.com>
2021-04-06 21:46:40 -0700

    lkkl

--- request.php
+++ request.php
@@ -1,6 +1,7 @@
 <?php
 if(isset($_POST['boxColor']))
 {
+    echo "lklk";
     setcookie("boxColor", $_POST['boxColor'], time() + (86400 * 8), "/") or die('unable to create cookie');   
     echo $_COOKIE["boxColor"];
 }
