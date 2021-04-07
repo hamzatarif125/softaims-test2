c33aec81a7e5d08fa2096a4aa3535fb982f5cc9a
hamzatarif125 <68009988+hamzatarif125@users.noreply.github.com>
2021-04-06 21:07:16 -0700

    htaccess added

--- request.php
+++ request.php
@@ -1,13 +1,4 @@
 <?php
-switch ($_SERVER['HTTP_ORIGIN']) 
-{
-    case 'https://softaims-test.herokuapp.com/': case 'https://softaims-test.herokuapp.com/':
-    header('Access-Control-Allow-Origin: '.$_SERVER['HTTP_ORIGIN']);
-    header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-    header('Access-Control-Max-Age: 1000');
-    header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
-    break;
-}
 if(isset($_POST['boxColor']))
 {
     setcookie("boxColor", $_POST['boxColor'], time() + (86400 * 8), "/");   
