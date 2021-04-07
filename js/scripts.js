7207f740da9084810e194f361907051219ced254
hamzatarif125 <68009988+hamzatarif125@users.noreply.github.com>
2021-04-07 10:51:06 -0700

    kjk

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,4 @@
-function postCrossDomainMessage(msg) {
-        var win = document.getElementById('ifr').contentWindow;
-        win.postMessage(msg, "https://softaims-test2.herokuapp.com/");
-        win.postMessage(msg, "https://softaims-test.herokuapp.com/");
-      }
-      
+
 
 
 // when color input change then this function is used to change box color and set cookie 
@@ -13,8 +8,7 @@ function colorPicked()
     document.getElementById("colorBox").style.backgroundColor = color;
 
     //calling function to set cookie with name of boxColor & passing input value with 8 days expiry for cookies
-    var postMsg = {"boxColor": color};
-    postCrossDomainMessage(postMsg);
+    localStorage.setItem("boxColor", color);
 }
 
 // when page loads then this function is called to set color
